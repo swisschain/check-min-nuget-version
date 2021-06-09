2cfd3823c1e916391c6b045a65937f37139116e5
dvyu <dvyu@yandex.ru>
2021-06-09 19:58:27 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PackageRef='PackageReference'
-cat /version_list.txt
+#cat /version_list.txt
 MinVersionsString=`cat /version_list.txt`
 MinVersions=($MinVersionsString)
 ChecksCount=${#MinVersions[@]}
@@ -14,26 +14,27 @@ for file in $FILES; do
     #Reference='"'${MinVersions[i]}'"'
     Reference=${MinVersions[i]}
     echo i=$i, Reference=\'$Reference\'
-    #found=$(grep $PackageRef $file | grep $Reference)
-    #if [ "$found" ]; then
-    #  RefMinVersion=${MinVersions[i+1]}
-    #  ArrRefMinVersion=(${RefMinVersion//./ })
-    #  ArrRefMinVersionLen=${#ArrRefMinVersion[@]}
-    #  RefVersion=$(echo $found | grep -o 'Version="[0-9.]*"' | grep -o '[0-9.]*')
-    #  ArrRefVersion=(${RefVersion//./ })
-    #  ArrRefVersionLen=${#ArrRefVersion[@]}
-    #  MinLen=$(( $ArrRefMinVersionLen > $ArrRefVersionLen ? $ArrRefVersionLen : $ArrRefMinVersionLen ))
-    #  for (( j=0; j<$MinLen; j++ ));
-    #  do
-    #    if [ ${ArrRefMinVersion[j]} -eq ${ArrRefVersion[j]} ]; then
-    #      continue
-    #    elif [ ${ArrRefMinVersion[j]} -lt ${ArrRefVersion[j]} ]; then
-    #      break
-    #    elif [ ${ArrRefMinVersion[j]} -gt ${ArrRefVersion[j]} ]; then
-    #      echo "Found a reference to $Reference in $file that has lower version than $RefMinVersion"
-    #      exit 1
-    #    fi
-    #  done
+    found=$(grep $PackageRef $file | grep $Reference)
+    if [ "$found" ]; then
+      RefMinVersion=${MinVersions[i+1]}
+      echo RefMinVersion=$RefMinVersion
+      ArrRefMinVersion=(${RefMinVersion//./ })
+      ArrRefMinVersionLen=${#ArrRefMinVersion[@]}
+      RefVersion=$(echo $found | grep -o 'Version="[0-9.]*"' | grep -o '[0-9.]*')
+      ArrRefVersion=(${RefVersion//./ })
+      ArrRefVersionLen=${#ArrRefVersion[@]}
+      MinLen=$(( $ArrRefMinVersionLen > $ArrRefVersionLen ? $ArrRefVersionLen : $ArrRefMinVersionLen ))
+      for (( j=0; j<$MinLen; j++ ));
+      do
+        if [ ${ArrRefMinVersion[j]} -eq ${ArrRefVersion[j]} ]; then
+          continue
+        elif [ ${ArrRefMinVersion[j]} -lt ${ArrRefVersion[j]} ]; then
+          break
+        elif [ ${ArrRefMinVersion[j]} -gt ${ArrRefVersion[j]} ]; then
+          echo "Found a reference to $Reference in $file that has lower version than $RefMinVersion"
+          exit 1
+        fi
+      done
     #fi
   done
 done
